af5a99a06fbb2d1e8092708ba89881d27c7c9f10
jiaozhu <jiaozhu@asrmicro.com>
2017-01-22 16:38:53 +0800

    add  say_goodbye func.
    Change-Id: Ia46908ea3f910529bdcf27ae3748fb4bbe0879c2

--- shell.py
+++ shell.py
@@ -11,7 +11,13 @@ say_hi()
     echo $1" say hi to all:)"
 }
 
+say_bye()
+{
+    echo $1" say goodbye to all:("
+}
+
 echo_msg "Hello, "
 echo_msg "Welcome to git new world!"
 
 say_hi Jack
+say_bye Jack
